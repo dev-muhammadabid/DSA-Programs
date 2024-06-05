47afe755cd4c038ada965ee98b943bab063c2a6c
dev-muhammadabid <muhammadabid.dev@gmail.com>
2024-06-05 11:06:37 +0530

    Commit

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,17 +1,35 @@
+import java.util.*;
 public class Recursion {
+    public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz" };
 
+    public static void printComb(String str, int index, String comb){
+        if (index == str.length()){
+            System.out.println(comb);
+            return;
+        }
+
+        char currChar = str.charAt(index);
+        String mapping = keypad[currChar - '0'];
+
+        for (int i = 0; i < mapping.length(); i++){
+            printComb(str, index + 1, comb + mapping.charAt(i));
+        }
+
+    }
     public static void main(String[] args){
-        System.out.println("Recursion");
+        String str = "23";
+        printComb(str, 0, "");
+
     }
 }
 
 /*
-Print Number 5 to 1 in Recursion
+1. Print Number 5 to 1 in Recursion
 
 public static void printNum(int n){
         if(n == 0){
            return;
-        }
+        } // Base Case
         System.out.println(n);
         printNum(n-1);
     }
@@ -25,7 +43,7 @@ public static void printNum(int n){
 
 
 /*
-Print Number 1 to 5 in Recursion
+2. Print Number 1 to 5 in Recursion
 
 public static void printNum(int n){
         if(n == 6){
@@ -44,7 +62,7 @@ public static void printNum(int n){
 
 
 /*
-Print the sum of n Natural Numbers
+3. Print the sum of n Natural Numbers
 
 public static void sumOfNaturalNum(int i, int num, int sum ){
         if (i == num) {
@@ -65,7 +83,7 @@ public static void sumOfNaturalNum(int i, int num, int sum ){
 
 
 /*
-Print the sum of n Factorial
+4. Print the sum of n Factorial
 
  public static int sumOfNum(int n){
         if (n == 1 || n == 0){
@@ -86,7 +104,7 @@ Print the sum of n Factorial
 
 
 /*
-Print the Fibonacci Series
+5. Print the Fibonacci Series
 
  public static void printFib(int a, int b, int n){
         if (n == 0){
@@ -108,7 +126,7 @@ Print the Fibonacci Series
 
 
 /*
-Print the n^x value (stack height = n)
+6. Print the n^x value (stack height = n)
 
     public static int powerFunc(int x, int pow){
         if(x == 0){
@@ -134,7 +152,7 @@ Print the n^x value (stack height = n)
 
 
 /*
-Print the n^x value (stack height = logn)
+7. Print the n^x value (stack height = logn)
 
     public static int powerFunc(int x, int pow){
         if(x == 0){
@@ -157,5 +175,229 @@ Print the n^x value (stack height = logn)
         int pow = sc.nextInt();
         int ans = powerFunc(x, pow);
         System.out.println("The " + x + " ^ " + pow + " is " + ans);
+    }
+*/
+
+
+/*
+8. Tower Of Hanoi
+    Time Complexity = O(2^n)
+
+    public static void towerOfHanoi(int n, String src, String help, String dest ){
+        if (n == 1){
+            System.out.println("Transfer Disk" + n + " from " + src + " to " + dest);
+            return;
+        }
+        towerOfHanoi(n-1, src, dest, help);
+        System.out.println("Transfer Disk" + n + " from " + src + " to " + dest);
+        towerOfHanoi(n-1, help, src, dest);
+    }
+    public static void main(String[] args){
+        System.out.println("Recursion Program");
+        int n = 3;
+        towerOfHanoi(n, "S", "H", "D");
+    }
+*/
+
+
+/*
+9. Reverse String
+
+     if ( index == 0){
+            System.out.println(str.charAt(index));
+            return;
+        }
+        System.out.println(str.charAt(index));
+        revString(str,index-1);
+    }
+    public static void main(String[] args){
+        String str = "abcd";
+        revString(str, str.length()-1);
+
+    }
+*/
+
+
+/*
+10. Find the 1st & last occurance of an element in string
+Time Complexity = O(n)
+
+     public static int first = -1;
+    public static int last = -1;
+    public static void findOccurance(String str, int index, char element){
+        if (index == str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+        char currChar = str.charAt(index);
+        if(currChar == element){
+            if (first == -1){
+                first = index;
+            }else{
+                last = index;
+            }
+        }
+        findOccurance(str, index+1, element);
+    }
+    public static void main(String[] args){
+        String str = "abaacdaefaah";
+        findOccurance(str, 0, 'a' );
+    }
+*/
+
+
+/*
+11. Check if an array is sorted(Strictly Increasing)
+Time Complexity = O(n)
+
+ public static boolean sortArray(int arr[], int index){
+        if (index == arr.length-1){
+            return true;
+        }
+        if (arr[index] < arr[index + 1]){
+            return sortArray(arr, index+1);
+        }else {
+            return false;
+        }
+    }
+    public static void main(String[] args){
+        int arr[] = {1,3,2,8};
+        System.out.println(sortArray(arr, 0));
+    }
+*/
+
+
+/*
+12. Move all 'x' to the end of the string
+Time Complexity = O(n)
+
+ public static void moveAllx(String str, int index, int count, String newStr){
+        if(index == str.length()){
+            for (int i = 0; i < count ; i++) {
+                newStr += 'x';
+            }
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(index);
+        if (currChar == 'x'){
+            count++;
+            moveAllx(str, index+1, count, newStr);
+        }else{
+            newStr += currChar;
+            moveAllx(str, index+1, count, newStr);
+        }
+    }
+    public static void main(String[] args){
+        String str = "axbcxxd";
+        moveAllx(str, 0, 0, "");
+    }
+*/
+
+
+/*
+13. Remove duplicates in a string
+Time Complexity = O(n)
+
+ public static boolean[] map = new boolean[26];
+
+    public static void removeDupli(String str, int index, String newStr){
+        if (index == str.length()){
+            System.out.println(newStr);
+            return;
+        }
+
+        char currChar = str.charAt(index);
+        if (map[currChar - 'a']){
+            removeDupli(str, index + 1, newStr);
+        }else{
+            newStr +=currChar;
+            map[currChar - 'a'] = true;
+            removeDupli(str, index + 1, newStr);
+        }
+    }
+    public static void main(String[] args){
+        String str = "abbccda";
+        removeDupli(str, 0, "");
+
+    }
+*/
+
+
+/*
+13. Print all the subsequences of a string   //IMPORTANT
+Time Complexity = O(2^n)
+
+public static void sunSeq(String str, int index, String newStr){
+        if (index == str.length()){
+            System.out.println(newStr);
+            return;
+        }
+
+        char currChar = str.charAt(index);
+        sunSeq(str, index + 1, newStr+currChar);
+        sunSeq(str, index + 1, newStr);
+    }
+    public static void main(String[] args){
+        String str = "abc";
+        sunSeq(str, 0, "");
+
+    }
+*/
+
+
+/*
+14. Print all the unique subsequences of a string   //IMPORTANT
+Time Complexity = O(2^n)
+
+public static void sunSeq(String str, int index, String newStr, HashSet<String> set){
+        if (index == str.length()){
+            if (set.contains(newStr)){
+                return;
+            }else {
+                System.out.println(newStr);
+                set.add(newStr);
+                return;
+            }
+        }
+
+        char currChar = str.charAt(index);
+        sunSeq(str, index + 1, newStr+currChar, set);
+        sunSeq(str, index + 1, newStr, set);
+    }
+    public static void main(String[] args){
+        String str = "aaa";
+        HashSet<String> set = new HashSet<>();
+        sunSeq(str, 0, "", set);
+
+    }
+*/
+
+
+/*
+15. Print keypad combinations   //Keypad Mobile
+Time Complexity = O(4^n)
+
+public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz" };
+
+    public static void printComb(String str, int index, String comb){
+        if (index == str.length()){
+            System.out.println(comb);
+            return;
+        }
+
+        char currChar = str.charAt(index);
+        String mapping = keypad[currChar - '0'];
+
+        for (int i = 0; i < mapping.length(); i++){
+            printComb(str, index + 1, comb + mapping.charAt(i));
+        }
+
+    }
+    public static void main(String[] args){
+        String str = "23";
+        printComb(str, 0, "");
+
     }
 */
