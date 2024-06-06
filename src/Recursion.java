026ef1f93552789250b0167d242dcc446bcb644b
dev-muhammadabid <muhammadabid.dev@gmail.com>
2024-06-06 20:50:56 +0530

    Commit Recursion

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,25 +1,8 @@
 import java.util.*;
 public class Recursion {
-    public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz" };
-
-    public static void printComb(String str, int index, String comb){
-        if (index == str.length()){
-            System.out.println(comb);
-            return;
-        }
-
-        char currChar = str.charAt(index);
-        String mapping = keypad[currChar - '0'];
-
-        for (int i = 0; i < mapping.length(); i++){
-            printComb(str, index + 1, comb + mapping.charAt(i));
-        }
-
-    }
     public static void main(String[] args){
-        String str = "23";
-        printComb(str, 0, "");
 
+        System.out.println("Recursion Program");
     }
 }
 
@@ -399,5 +382,123 @@ public static String[] keypad = {".", "abc", "def", "ghi", "jkl", "mno", "pqrs",
         String str = "23";
         printComb(str, 0, "");
 
+    }
+*/
+
+
+/*
+16. Print all permutations of a string   //IMPORTANT
+Time Complexity = O(n!)
+
+public static void printPerm(String str, String permutation){
+        if (str.length() == 0){
+            System.out.println(permutation);
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++ ){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            printPerm(newStr, permutation+currChar);
+        }
+    }
+    public static void main(String[] args){
+        String str = "abc";
+        printPerm(str, "");
+    }
+*/
+
+
+/*
+17. Count total paths in a maze to move from (0,0) to (n,m) // IMPORTANT
+
+public static int countPaths(int i, int j, int n, int m){
+        if (i == n || j == m){
+            return 0;
+        }
+        if (i == n-1 && j == m-1){
+            return 1;
+        }
+        int downPaths = countPaths(i+1, j, n, m);
+        int rightPaths = countPaths(i, j+1, n, m);
+
+        return downPaths + rightPaths;
+    }
+    public static void main(String[] args){
+        int n = 3, m =3;
+        int totalPaths = countPaths(0, 0, n, m);
+        System.out.println(totalPaths);
+    }
+*/
+
+
+/*
+18. Place Tiles of size 1xm in a floor of size nxm // IMPORTANT
+
+public static int placeTiles(int n, int m){
+        if (n == m){
+            return 2;
+        }
+        if (n < m){
+            return 1;
+        }
+        int vertPlacements = placeTiles(n-m, m);
+        int horzPlacements = placeTiles(n-1, m);
+
+        return vertPlacements + horzPlacements;
+    }
+    public static void main(String[] args){
+        int n = 4, m =2;
+        System.out.println(placeTiles(n, m));
+    }
+*/
+
+
+/*
+19. Find the number of ways in which you can invite n people to your party, single or in pairs // IMPORTANT
+
+    public static int callGuests(int n){
+        if (n <= 1){
+            return 1;
+        }
+        int way1 = callGuests(n-1);
+        int way2 = (n-1) * callGuests(n-2);
+
+        return way1 + way2;
+    }
+    public static void main(String[] args){
+        int n = 4;
+        System.out.println(callGuests(n));
+    }
+*/
+
+
+/*
+20. Print all the subsets of a set of first n natural numbers // IMPORTANT
+Time Complexity = O(2^n)
+
+        public static void  printSubset(ArrayList<Integer> subset){
+        for (int i = 0; i< subset.size(); i++){
+            System.out.print(subset.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public static void findSubsets(int n, ArrayList<Integer> subset){
+        if (n == 0){
+            printSubset(subset);
+            return;
+        }
+
+        subset.add(n);
+        findSubsets(n-1, subset);
+
+        subset.remove(subset.size()-1);
+        findSubsets(n-1, subset);
+    }
+    public static void main(String[] args){
+        int n = 3;
+        ArrayList<Integer> subset = new ArrayList<>();
+        findSubsets(n, subset);
     }
 */
