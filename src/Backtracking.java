a60b2009b9fba9dd5a18eb547623f3b3108d6e45
dev-muhammadabid <muhammadabid.dev@gmail.com>
2024-06-09 10:31:32 +0530

    ADD PROGRAMS

--- src/Backtracking.java
+++ src/Backtracking.java
@@ -1,12 +1,9 @@
 import java.util.*;
-
 public class Backtracking {
     public static void main(String[] args){
-        System.out.println("Backtracking");
+        System.out.println("Backtracking Important Programs");
     }
-
 }
-
 
 
 
@@ -144,4 +141,104 @@ public boolean isSafe(int row, int col, char[][] board) {
             System.out.println();
         }
     }
+*/
+
+
+/*
+3. Sudoku Solver
+Time Complexity = O(n*n!)
+
+import java.util.*;
+
+public class Backtracking {
+
+    // Checks if placing a number at board[row][col] is safe
+    public boolean isSafe(char[][] board, int row, int col, int number) {
+        // Check the row and column
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == (char) (number + '0')) {
+                return false;
+            }
+            if (board[row][i] == (char) (number + '0')) {
+                return false;
+            }
+        }
+
+        // Check the 3x3 grid
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (board[i][j] == (char) (number + '0')) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Helper method to solve the Sudoku board
+    public boolean helper(char[][] board, int row, int col) {
+        if (row == board.length) {
+            return true;
+        }
+
+        int nrow = 0;
+        int ncol = 0;
+        if (col != board.length - 1) {
+            nrow = row;
+            ncol = col + 1;
+        } else {
+            nrow = row + 1;
+            ncol = 0;
+        }
+
+        if (board[row][col] != '.') {
+            return helper(board, nrow, ncol);
+        } else {
+            for (int i = 1; i <= 9; i++) {
+                if (isSafe(board, row, col, i)) {
+                    board[row][col] = (char) (i + '0');
+                    if (helper(board, nrow, ncol)) {
+                        return true;
+                    } else {
+                        board[row][col] = '.';
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    // Solves the Sudoku board
+    public void solveSudoku(char[][] board) {
+        helper(board, 0, 0);
+    }
+
+    public static void main(String[] args) {
+        Backtracking solver = new Backtracking();
+        char[][] board = {
+            {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+            {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+            {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+            {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+            {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+            {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+            {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+            {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+            {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        solver.solveSudoku(board);
+
+        // Print the solved board
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
 */
