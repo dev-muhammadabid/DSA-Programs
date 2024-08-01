ff476b7bc59d5ce4b9c6b24295c8add597a46fe3
dev-muhammadabid <muhammadabid.dev@gmail.com>
2024-08-01 21:15:19 +0530

    ADD ArrayList

---
+++ src/ArrayLists.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class ArrayLists {
+    public static void main(String[] args) {
+        System.out.println("ArrayList");
+    }
+}
+
+/*
+        //Define
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        //ADD Element
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(4);
+        System.out.println(list);
+
+
+        //GET Element
+        int element = list.get(1);
+        System.out.println(element);
+
+
+        //ADD Element in Between
+        list.add(1, 2);
+        System.out.println(list);
+
+
+        //SET Element
+        list.set(0,1);
+        System.out.println(list);
+
+
+        //Delete Element
+        list.remove(1);
+        System.out.println(list);
+
+
+        //Check Size of the array
+        int size = list.size();
+        System.out.println(size);
+
+
+        //LOOPs
+        for (int i = 0; i < list.size() ; i++) {
+            System.out.print(list.get(i));
+        }
+        System.out.println();
+
+
+        //SORTING
+        Collections.sort(list);
+        System.out.println(list);
+
+*/
